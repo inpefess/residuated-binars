13260874b3e71be5e594c438e80075c42e43eb8d
Boris Shminke <boris@shminke.ml>
2021-10-29 14:52:26 +0200

    increase coverage threshold

--- show_report.sh
+++ show_report.sh
@@ -6,7 +6,7 @@ pycodestyle --max-doc-length 1000 --ignore E203,E501,W503 \
 	    ${PACKAGE_NAME}
 pylint --rcfile=.pylintrc ${PACKAGE_NAME}
 mypy --config-file mypy.ini ${PACKAGE_NAME}
-pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=82 \
+pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=87 \
        --junit-xml test-results/residuated_binars.xml \
        --doctest-modules ${PACKAGE_NAME} tests
 scc -i py ${PACKAGE_NAME}
