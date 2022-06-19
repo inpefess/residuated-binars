120769e8853db5beb343440dd81b9f55d482b77e
Boris Shminke <boris@shminke.ml>
2022-06-19 23:42:48 +0200

    local-build and CI should do the same

--- local-build.sh
+++ local-build.sh
@@ -9,5 +9,5 @@ pydocstyle ${PACKAGE_NAME} tests
 flake8 ${PACKAGE_NAME} tests
 pylint ${PACKAGE_NAME} tests
 mypy ${PACKAGE_NAME} tests
-pytest --cov-report term-missing ${PACKAGE_NAME} tests
+pytest
 scc -i py ${PACKAGE_NAME}
